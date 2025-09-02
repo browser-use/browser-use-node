ab609797227701109ee1175268976d12f3d1b5fe
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-09-02 11:05:15 +0000

    SDK regeneration

--- src/Client.ts
+++ src/Client.ts
@@ -50,8 +50,8 @@ export class BrowserUseClient {
                 {
                     "X-Fern-Language": "JavaScript",
                     "X-Fern-SDK-Name": "browser-use-sdk",
-                    "X-Fern-SDK-Version": "2.0.0-rc1",
-                    "User-Agent": "browser-use-sdk/2.0.0-rc1",
+                    "X-Fern-SDK-Version": "1.2.1",
+                    "User-Agent": "browser-use-sdk/1.2.1",
                     "X-Fern-Runtime": core.RUNTIME.type,
                     "X-Fern-Runtime-Version": core.RUNTIME.version,
                 },
